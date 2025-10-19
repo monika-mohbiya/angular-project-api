733b090f058ebb5563228b357d341c04820514e9
Monika <monikamohbiya777@gmail.com>
2025-10-19 20:30:59 +0530

    code push

--- src/controllers/dbconnection.js
+++ src/controllers/dbconnection.js
@@ -1,17 +1,37 @@
 // dbconnection.js
-require('dotenv').config();
-const mysql = require('mysql2');
+// require('dotenv').config();
+// const mysql = require('mysql2');
 
-const con = mysql.createConnection({
+// const con = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// con.connect((err) => {
+//     if (err) throw err;
+//     console.log("Connected to MySQL!");
+// });
+
+// module.exports = con;
+
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
-con.connect((err) => {
-    if (err) throw err;
-    console.log("Connected to MySQL!");
+db.connect(err => {
+    if (err) console.error('❌ DB connection failed:', err.message);
+    else console.log('✅ Connected to Clever Cloud MySQL!');
 });
 
-module.exports = con;
+export default db;
+
